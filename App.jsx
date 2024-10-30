30b55c731a752dcb02c89602f06c17a279166799
BhanuPrasad404 <bhanuprasadgummidelli@gmail.com>
2024-10-30 10:04:24 +0530

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,274 @@
+import Timeapp from "./Timeapp";
+
+function App() {
+
+  return(<Timeapp></Timeapp>)
+}
+
+export default App
+//import StopWatch from "./StopWatch";
+
+//function App() {
+
+ // return(
+ //   <StopWatch></StopWatch>
+ // )
+//}
+
+//export default App
+/*---import  UseRef2 from './UseRef2.jsx' 
+function App() {
+
+  return(
+    <>
+    <UseRef2></UseRef2>
+    </>
+  )
+}
+export default App---*/
+/*--import UseRef from "./UseRef";;
+
+function App() {
+
+  return(
+    <UseRef></UseRef>
+  )
+}
+
+export default App;--*/
+/*--import  ComponentA from "./ComponentA.jsx"
+
+function App() {
+
+  return(
+    <ComponentA></ComponentA>
+  )
+}
+
+export default App--*/
+/*----import Window from './Window.jsx' 
+
+function App() {
+
+  return(<Window></Window>)
+}
+
+export default App;-----*/
+/*---import UseEffect  from './UseEffect.jsx';
+
+function App() {
+
+  return(<UseEffect></UseEffect>)
+}
+
+export default App;---*/
+/*---import Todolistapp from './Todolistapp.jsx'
+
+function App(){
+
+  return(
+  <Todolistapp/>
+  )
+}
+
+export default App;---*/
+/*----import Carhandle from './Carhandle.jsx';
+
+function App() {
+
+  return(<Carhandle></Carhandle>)
+}
+
+export default App---*/
+/*----import FruitList from './FruitList.jsx';
+
+function App() {
+
+  return(<FruitList></FruitList>)
+}
+
+export default App;---*/
+/*----import Updatearray from './Updatearray.jsx';
+
+function App() {
+
+  return(<Updatearray></Updatearray>)
+}
+
+export default App---*/
+
+/*---import Updatestate from './Updatestate.jsx'
+
+function App() {
+
+  return(<Updatestate></Updatestate>)
+
+}
+
+export default App ---*/
+/*---import Updater from './Updater.jsx';
+
+function App() {
+
+  return(<Updater></Updater>)
+}
+
+export default App ---*/
+/*---import Colorpick from './Colorpick.jsx';
+
+function App() {
+
+  return(<Colorpick/>)
+}
+
+export default App ----*/
+
+/*---import Onchange from './Onchange.jsx'
+function App() {
+ 
+  return(
+    <>
+    <Onchange></Onchange>
+    
+    </>
+  )
+
+
+
+}
+export default App ---*/
+
+/*--import Counter from './Counter.jsx'
+
+function App() {
+  return(
+    <>
+     <Counter></Counter>
+    
+    </>
+  )
+
+
+}
+export default App ---*/
+/*--import Hooks from './Hooks.jsx'
+
+function App() {
+
+  return(
+    <> 
+    <Hooks></Hooks>
+    
+    </>
+  )     
+
+}
+
+export default App---*/
+
+/*--import React from 'react'
+import Btn from './Btn.jsx'
+import Img from './Img.jsx'
+function App() {
+    
+  return(
+    <>
+      <Img></Img>
+      <Btn></Btn>
+     
+    
+    </>
+  );
+
+
+}
+export default App --*/
+
+
+/*---import React from 'react'
+import List from './List.jsx'
+function App() {
+  const fruits = [{ id: 1, name: "apple", calories: 90 },
+  { id: 2, name: "banana", calories: 80 },
+  { id: 3, name: "orange", calories: 110 },
+  { id: 4, name: "grapes", calories: 150 },
+  { id: 5, name: "watermelon", calories: 130 }];
+
+  const veg = [{ id: 1, name: "apple", calories: 90 },
+    { id: 2, name: "banana", calories: 80 },
+    { id: 3, name: "orange", calories: 110 },
+    { id: 4, name: "grapes", calories: 150 },
+    { id: 5, name: "watermelon", calories: 130 }];
+  
+
+  return (<>
+
+    { fruits.length>0&&   <List item={fruits} catogory = "Fruits"/>}
+    { veg.length >0&&   <List item={veg} catogory = "Fruits"/>}
+  </>);
+
+
+}
+export default App ---*/
+
+/*--import  Condrender from './Condrender.jsx';
+
+function App(){
+
+  return(
+
+    <>
+      <Condrender isLoggedIn = {true} username = "Spongebob"/>
+       
+    
+    </>
+  )
+}
+export default App ---*/
+/*---import React from 'react';
+
+
+import Student  from "./student.jsx";
+function App(){ 
+
+    return(
+        <>
+          <Student name ="bob" age = {10} location = "Hyderabad"/>
+          <Student name = "sandy" age = {15} location = "chennai"/>
+          <Student name = "prop"  age = {16} location = "mumbai"/>
+          <Student/>
+
+          
+        </>    
+    );
+}
+export default App ----*/
+/*--import Button  from './Button.jsx';
+
+function App(){
+
+    return(
+        <Button />
+    ); 
+
+
+}
+
+export default App; ---*/
+
+
+/*--import Card from './Card.jsx'
+function App() {
+
+  return (
+    <>
+      <Card></Card>
+      <Card></Card>
+      <Card></Card>
+      <Card></Card>         
+    </>
+  )
+}
+
+export default App
+---*/
